957d8645df74e4e6d55f0226b0586d7c9f36a7e6
siarez <sia@siarez.com>
2020-03-17 17:11:49 -0700

    dataloader workers as argument

--- train_cifar.py
+++ train_cifar.py
@@ -18,6 +18,7 @@ parser.add_argument('--mom', default=0.9, type=float, help='Momentum')
 parser.add_argument('--optim', default='adam', choices=['sgd', 'adam'], help='momentum')
 parser.add_argument('--batch', default=128, type=int, help='Batch size')
 parser.add_argument('--epochs', default=100, type=int, help='Epochs to train for')
+parser.add_argument('--workers', default=4, type=int, help='Dataloader workers')
 parser.add_argument('--normal', action='store_true', default=False, help='Use pytorch\'s conv layer')
 parser.add_argument('--model', choices=['VGG_tiny', 'VGG_mini', 'VGG11', 'VGG13', 'VGG16', 'VGG19', 'sp1'], default='VGG_tiny', help='Pick a VGG')
 args = parser.parse_args()
@@ -43,10 +44,10 @@ transform_test = transforms.Compose([
 ])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch, shuffle=True, num_workers=4)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=args.batch, shuffle=True, num_workers=args.workers)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch, shuffle=False, num_workers=4)
+testloader = torch.utils.data.DataLoader(testset, batch_size=args.batch, shuffle=False, num_workers=args.workers)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
