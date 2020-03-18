2561cb41cffee5417e79264c89770fa8ef287a82
siarez <sia@siarez.com>
2020-03-18 00:17:47 -0400

    bug fix

--- custom_layers.py
+++ custom_layers.py
@@ -90,7 +90,7 @@ class Conv2DFunctionCustom(Function):
         if ctx.needs_input_grad[0]:
             if conf['topk']:
                 topk_idx = grad_weight.abs().sum((2, 3)).topk(grad_weight.shape[1] // 2, dim=1)[1]
-                topk_mask = torch.ones(weight.shape[0], weight.shape[1]).scatter(1, topk_idx, torch.zeros_like(topk_idx, dtype=torch.float))
+                topk_mask = torch.ones((weight.shape[0], weight.shape[1]), device=weight.device).scatter(1, topk_idx, torch.zeros_like(topk_idx, dtype=torch.float))
                 topk_mask = topk_mask.unsqueeze(-1).unsqueeze(-1)
                 bw_weight = weight * topk_mask
             else:
